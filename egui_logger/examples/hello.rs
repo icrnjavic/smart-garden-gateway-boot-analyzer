83d1074077f4376ada64765727f84e388fac2317
icrnjavic <32548477+icrnjavic@users.noreply.github.com>
2024-03-04 16:56:28 +0100

    GUI update to modified egui_logger

---
+++ egui_logger/examples/hello.rs
@@ -0,0 +1,41 @@
+use eframe::NativeOptions;
+
+fn main() {
+    // Initilize the logger
+    egui_logger::init_with_max_level(log::LevelFilter::Debug).expect("Error initializing logger");
+
+    let options = NativeOptions::default();
+
+    eframe::run_native(
+        "egui_logger",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    )
+    .unwrap();
+}
+
+#[derive(Default)]
+struct MyApp;
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if ui.button("This produces Debug Info").clicked() {
+                log::debug!("Very verbose Debug Info")
+            }
+            if ui.button("This produces an Info").clicked() {
+                log::info!("Some Info");
+            }
+            if ui.button("This produces an Error").clicked() {
+                log::error!("Error doing Something");
+            }
+            if ui.button("This produces a Warning").clicked() {
+                log::warn!("Warn about something")
+            }
+        });
+        egui::Window::new("Log").show(ctx, |ui| {
+            // draws the actual logger ui
+            egui_logger::logger_ui(ui);
+        });
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod analyzer;
+pub mod config;
+pub mod jig;
